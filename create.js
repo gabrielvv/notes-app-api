6098255f7d3a73fa495764a4332c6a8d3687e607
Gabriel Vautrin <gvautrin@serial.ch>
2020-03-02 14:59:47 +0100

    feat: create function

---
+++ create.js
@@ -0,0 +1,47 @@
+import uuid from "uuid";
+import AWS from "aws-sdk";
+import debug from "debug";
+
+const dbg = debug("aws");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export function main (event, context, callback) {
+    const data = JSON.parse(event.body);
+
+    const params = {
+        TableName: process.env.tableName,
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now(),
+        }
+    };
+
+    dynamoDb.put(params, (error, data) => {
+        const headers = {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        };
+
+        if(error) {
+            dbg(error);
+            const response = {
+                statusCode: 500,
+                headers,
+                body: JSON.stringify({ status: false })
+            };
+            callback(null, response);
+            return;
+        }
+
+        const response = {
+            statusCode: 200,
+            headers,
+            body: JSON.stringify(params.Item)
+        };
+        callback(null, response);
+    });
+}
